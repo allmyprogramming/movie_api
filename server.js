e1c677115d42a775f8f2ccb592c27b51ee9ef154
allmyprogramming <88310265+allmyprogramming@users.noreply.github.com>
2025-02-02 20:27:21 +0100

    removed server.js file

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-// Create a simple HTTP server
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'}); // Set response header
-  response.end('Welcome to my book club!\n'); // Send a response message
-}).listen(8080); // Listen on port 8080
-
-console.log('My first Node test server is running on Port 8080.');

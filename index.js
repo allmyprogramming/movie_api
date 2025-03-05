3daa996a57cf93e38d5a3a44c037e4be454edc64
allmyprogramming <88310265+allmyprogramming@users.noreply.github.com>
2025-03-05 14:25:29 +0100

    checking cors

--- index.js
+++ index.js
@@ -20,13 +20,18 @@ let allowedOrigins = ['https://movie-api-lvgy.onrender.com', 'http://localhost:1
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+    if (allowedOrigins.indexOf(origin) === -1) {
       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
       return callback(new Error(message), false);
     }
     return callback(null, true);
-  }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
+// Handle preflight requests
+app.options('*', cors());
 
 // Database Connection
 const dbURI = process.env.MONGODB_URI;

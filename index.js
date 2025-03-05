82c9eeab44b068b1255571a2b9b812ff390d0288
allmyprogramming <88310265+allmyprogramming@users.noreply.github.com>
2025-03-05 14:33:53 +0100

    udate to cores

--- index.js
+++ index.js
@@ -21,12 +21,14 @@ let allowedOrigins = ['https://movie-api-lvgy.onrender.com'];
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+    if (allowedOrigins.indexOf(origin) === -1) {
       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
       return callback(new Error(message), false);
     }
     return callback(null, true);
-  }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 // Database Connection
